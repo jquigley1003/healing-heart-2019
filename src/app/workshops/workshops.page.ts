af11432ce0d10fcdd8d1dc67c14a969cffe8d0fc
jquigley1003 <jeff.quigley@wavinghi.com>
2021-10-19 15:32:00 -0400

    added AnimationEvent to components - breaking change in Angular12

--- src/app/workshops/workshops.page.ts
+++ src/app/workshops/workshops.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { AnimationEvent, animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Router } from '@angular/router';
 
 @Component({
