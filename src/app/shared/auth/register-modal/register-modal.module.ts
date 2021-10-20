145f19502be377af0e5b7a8c1571c81e4e8f0e4a
jquigley1003 <jeff.quigley@wavinghi.com>
2021-10-20 12:51:36 -0400

    add shared modules and components

---
+++ src/app/shared/auth/register-modal/register-modal.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { RegisterModalComponent } from './register-modal.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    RegisterModalComponent
+  ]
+})
+export class RegisterModalModule { }
---
+++ src/app/shared/auth/sign-in-modal/sign-in-modal.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SignInModalComponent } from './sign-in-modal.component';
+
+@NgModule({
+  declarations: [
+    SignInModalComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class SignInModalModule { }
