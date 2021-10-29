d8981e276e5dd07487334fdc18169a7f19c922ba
jquigley1003 <jeff.quigley@wavinghi.com>
2021-10-28 21:05:33 -0400

    add content to module 2

--- src/app/workshops/marrying-yourself/module02/module02.page.ts
+++ src/app/workshops/marrying-yourself/module02/module02.page.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+import { YoutubePlayerWeb } from 'capacitor-youtube-player'; // Web version
 
 @Component({
   selector: 'app-module02',
   templateUrl: './module02.page.html',
   styleUrls: ['./module02.page.scss'],
 })
-export class Module02Page implements OnInit {
+export class Module02Page implements OnInit, AfterViewInit {
+  completedModule: boolean;
+  showCompleteBtn: boolean;
+  showIncompleteBtn = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.initializeYoutubePlayerPluginWeb();
+  }
+
+  async initializeYoutubePlayerPluginWeb() {
+    const player1 = {playerId: 'youtube-player1', playerSize: {}, videoId: '46f6qgo7EYA'};
+    const result1 = await YoutubePlayerWeb.initialize(player1);
+    const player2 = {playerId: 'youtube-player2', playerSize: {}, videoId: 'r9VexoTZnrw'};
+    const result2 = await YoutubePlayerWeb.initialize(player2);
+    const player3 = {playerId: 'youtube-player3', playerSize: {}, videoId: 'C6R8P1hhgW4'};
+    const result3 = await YoutubePlayerWeb.initialize(player3);
+    const player4 = {playerId: 'youtube-player4', playerSize: {}, videoId: 'gJUrhzGejTM'};
+    const result4 = await YoutubePlayerWeb.initialize(player4);
+    const player5 = {playerId: 'youtube-player5', playerSize: {}, videoId: 'D4HxOinvaos'};
+    const result5 = await YoutubePlayerWeb.initialize(player5);
+    const player6 = {playerId: 'youtube-player6', playerSize: {}, videoId: 'IYzlVDlE72w'};
+    const result6 = await YoutubePlayerWeb.initialize(player6);
+    const player7 = {playerId: 'youtube-player7', playerSize: {}, videoId: 'iyBIidyRB-Q'};
+    const result7 = await YoutubePlayerWeb.initialize(player7);
+    const player8 = {playerId: 'youtube-player8', playerSize: {}, videoId: 'SmbmeOgWsqE'};
+    const result8 = await YoutubePlayerWeb.initialize(player8);
+  }
+
+  onToggle() {
+    console.log('completed is: ',this.completedModule);
+  }
+
+  markComplete() {
+    this.showCompleteBtn = true;
+    this.showIncompleteBtn = false;
+  }
+
+  markIncomplete() {
+    this.showCompleteBtn = false;
+    this.showIncompleteBtn = true;
   }
 
 }
