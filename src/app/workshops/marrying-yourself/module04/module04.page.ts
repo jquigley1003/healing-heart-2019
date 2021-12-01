bf0a5feb23601867079183ff814fb0f12777e58e
jquigley1003 <jeff.quigley@wavinghi.com>
2021-12-01 14:50:05 -0500

    finish adding content to module 4

--- src/app/workshops/marrying-yourself/module04/module04.page.ts
+++ src/app/workshops/marrying-yourself/module04/module04.page.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { YoutubePlayerWeb } from 'capacitor-youtube-player'; // Web version
+import Player from '@vimeo/player';
 
 @Component({
   selector: 'app-module04',
@@ -10,14 +11,23 @@ import { YoutubePlayerWeb } from 'capacitor-youtube-player'; // Web version
 export class Module04Page implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('firstAudio') firstAudioRef: ElementRef<HTMLAudioElement>;
   @ViewChild('principlesAudio') principlesAudioRef: ElementRef<HTMLAudioElement>;
-  @ViewChild('practice04Audio') practice04AudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('selfInquiryAudio') selfInquiryAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('engagementAudio') engagementAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('quoteAudio') quoteAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('dassyVimeo') dassyVimeoRef: ElementRef;
+  @ViewChild('devaEngagementVimeo') devaEngagementVimeoRef: ElementRef;
 
   completedModule: boolean;
   showCompleteBtn: boolean;
   showIncompleteBtn = true;
   audioDuration401: number;
   audioDuration402: number;
-  audioDuration403: number
+  audioDuration403: number;
+  audioDuration404: number;
+  audioDuration405: number;
+  dassyVimeoPlayer: Player;
+  devaEngagementVimeoPlayer: Player;
+  
 
   constructor() { }
 
@@ -32,18 +42,38 @@ export class Module04Page implements OnInit, AfterViewInit, OnDestroy {
     this.principlesAudioRef.nativeElement.onloadedmetadata = (event) => {
       this.audioDuration402 = this.principlesAudioRef.nativeElement.duration;
     };
-    this.practice04AudioRef.nativeElement.onloadedmetadata = (event) => {
-      this.audioDuration403 = this.practice04AudioRef.nativeElement.duration;
+    this.selfInquiryAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration403 = this.selfInquiryAudioRef.nativeElement.duration;
     };
+    this.engagementAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration404 = this.engagementAudioRef.nativeElement.duration;
+    };
+    this.quoteAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration405 = this.quoteAudioRef.nativeElement.duration;
+    };
+    this.dassyVimeoPlayer = new Player(this.dassyVimeoRef.nativeElement, {
+      id: 652064411,
+      height: 350
+    });
+    this.devaEngagementVimeoPlayer = new Player(this.devaEngagementVimeoRef.nativeElement, {
+      id: 652065200,
+      height: 350
+    });
   }
 
   async initializeYoutubePlayerPluginWeb() {
     const player1 = {playerId: 'youtube-player4-1', playerSize: {width: 640, height: 360}, videoId: 'bMpFmHSgC4Q'};
     const result1 = await YoutubePlayerWeb.initialize(player1);
+    const player2 = {playerId: 'youtube-player4-2', playerSize: {width: 640, height: 360}, videoId: 'LBk5I4nSXBo'};
+    const result2 = await YoutubePlayerWeb.initialize(player2);
+    const player3 = {playerId: 'youtube-player4-3', playerSize: {width: 640, height: 360}, videoId: 'aeKYip-sBjM'};
+    const result3 = await YoutubePlayerWeb.initialize(player3);
   }
 
   async destroyYoutubePlayerPluginWeb() {
-    const result1 = await YoutubePlayerWeb.destroy('youtube-player3-1');
+    const result1 = await YoutubePlayerWeb.destroy('youtube-player4-1');
+    const result2 = await YoutubePlayerWeb.destroy('youtube-player4-2');
+    const result3 = await YoutubePlayerWeb.destroy('youtube-player4-3');
   }
 
   onToggle() {
