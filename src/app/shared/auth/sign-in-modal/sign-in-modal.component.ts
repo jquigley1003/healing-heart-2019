b0850a767219580271ebc695fe734dafea84f421
jquigley1003 <jeff.quigley@wavinghi.com>
2021-12-14 14:04:00 -0500

    fixed email regex to allow captial letters

--- src/app/shared/auth/sign-in-modal/sign-in-modal.component.ts
+++ src/app/shared/auth/sign-in-modal/sign-in-modal.component.ts
@@ -28,7 +28,7 @@ export class SignInModalComponent implements OnInit {
     private router: Router
   ) {
     this.signInForm = this.formBuilder.group({
-      email: ['', (Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'))],
+      email: ['', (Validators.required, Validators.pattern('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$'))],
       password: ['', Validators.required]
     });
    }
