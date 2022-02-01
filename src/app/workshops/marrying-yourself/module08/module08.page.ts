3515c25b241339a3a1e9f8508536a28de6ce1b8e
jquigley1003 <jeff.quigley@wavinghi.com>
2022-01-31 20:54:13 -0500

    stop adding content with impact and energy

--- src/app/workshops/marrying-yourself/module08/module08.page.ts
+++ src/app/workshops/marrying-yourself/module08/module08.page.ts
@@ -13,8 +13,8 @@ export class Module08Page implements AfterViewInit, OnInit {
   @ViewChild('beingPresentAudio') beingPresentAudioRef: ElementRef<HTMLAudioElement>;
   @ViewChild('bsaoAudio') bsaoAudioRef: ElementRef<HTMLAudioElement>;
   @ViewChild('enjoymentAudio') enjoymentAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('transformingMindAudio') transformingMindAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('practiceTamingMindAudio') practiceTamingMindAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('livingTheDharmaAudio') livingTheDharmaAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('understandingYourImpactAudio') understandingYourImpactAudioRef: ElementRef<HTMLAudioElement>;
   // @ViewChild('embracingYourHeartAudio') embracingYourHeartAudioRef: ElementRef<HTMLAudioElement>;
   // @ViewChild('guidedImageryHeartAudio') guidedImageryHeartAudioRef: ElementRef<HTMLAudioElement>;
   // @ViewChild('practiceEmbracingHeartAudio') practiceEmbracingHeartAudioRef: ElementRef<HTMLAudioElement>;
@@ -31,8 +31,8 @@ export class Module08Page implements AfterViewInit, OnInit {
   audioDuration802: number;
   audioDuration803: number;
   audioDuration804: number;
-  audioDuration605: number;
-  audioDuration606: number;
+  audioDuration805: number;
+  audioDuration806: number;
   audioDuration607: number;
   audioDuration608: number;
   audioDuration609: number;
@@ -61,12 +61,12 @@ export class Module08Page implements AfterViewInit, OnInit {
     this.enjoymentAudioRef.nativeElement.onloadedmetadata = (event) => {
       this.audioDuration804 = this.enjoymentAudioRef.nativeElement.duration;
     };
-    // this.transformingMindAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration605 = this.transformingMindAudioRef.nativeElement.duration;
-    // };
-    // this.practiceTamingMindAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration606 = this.practiceTamingMindAudioRef.nativeElement.duration;
-    // };
+    this.livingTheDharmaAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration805 = this.livingTheDharmaAudioRef.nativeElement.duration;
+    };
+    this.understandingYourImpactAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration806 = this.understandingYourImpactAudioRef.nativeElement.duration;
+    };
     // this.embracingYourHeartAudioRef.nativeElement.onloadedmetadata = (event) => {
     //   this.audioDuration607 = this.embracingYourHeartAudioRef.nativeElement.duration;
     // };
@@ -110,12 +110,36 @@ export class Module08Page implements AfterViewInit, OnInit {
     const result2 = await YoutubePlayerWeb.initialize(player2);
     const player3 = {playerId: 'youtube-player0803', playerSize: {width: 640, height: 360}, videoId: 'HNBCVM4KbUM'};
     const result3 = await YoutubePlayerWeb.initialize(player3);
+    const player4 = {playerId: 'youtube-player0804', playerSize: {width: 640, height: 360}, videoId: 'ymPF0q7U5oM'};
+    const result4 = await YoutubePlayerWeb.initialize(player4);
+    const player5 = {playerId: 'youtube-player0805', playerSize: {width: 640, height: 360}, videoId: 'YkgkThdzX-8'};
+    const result5 = await YoutubePlayerWeb.initialize(player5);
+    const player6 = {playerId: 'youtube-player0806', playerSize: {width: 640, height: 360}, videoId: 'cef35Fk7YD8'};
+    const result6 = await YoutubePlayerWeb.initialize(player6);
+    const player7 = {playerId: 'youtube-player0807', playerSize: {width: 640, height: 360}, videoId: 't8AQOQzysjc'};
+    const result7 = await YoutubePlayerWeb.initialize(player7);
+    const player8 = {playerId: 'youtube-player0808', playerSize: {width: 640, height: 360}, videoId: 'QfpXSa3Au6I'};
+    const result8 = await YoutubePlayerWeb.initialize(player8);
+    const player9 = {playerId: 'youtube-player0809', playerSize: {width: 640, height: 360}, videoId: 'EHw170UPsyk'};
+    const result9 = await YoutubePlayerWeb.initialize(player9);
+    const player10 = {playerId: 'youtube-player0810', playerSize: {width: 640, height: 360}, videoId: 'KnXKqCmFBTw'};
+    const result10 = await YoutubePlayerWeb.initialize(player10);
+    const player11 = {playerId: 'youtube-player0811', playerSize: {width: 640, height: 360}, videoId: 'ynVTqHYg5A4'};
+    const result11 = await YoutubePlayerWeb.initialize(player11);
   }
 
   async destroyYoutubePlayerPluginWeb() {
     const result1 = await YoutubePlayerWeb.destroy('youtube-player0801');
     const result2 = await YoutubePlayerWeb.destroy('youtube-player0802');
     const result3 = await YoutubePlayerWeb.destroy('youtube-player0803');
+    const result4 = await YoutubePlayerWeb.destroy('youtube-player0804');
+    const result5 = await YoutubePlayerWeb.destroy('youtube-player0805');
+    const result6 = await YoutubePlayerWeb.destroy('youtube-player0806');
+    const result7 = await YoutubePlayerWeb.destroy('youtube-player0807');
+    const result8 = await YoutubePlayerWeb.destroy('youtube-player0808');
+    const result9 = await YoutubePlayerWeb.destroy('youtube-player0809');
+    const result10 = await YoutubePlayerWeb.destroy('youtube-player0810');
+    const result11 = await YoutubePlayerWeb.destroy('youtube-player0811');
   }
 
   onToggle() {
