35777d4cc5b3a8826d188f66c711ceb1d41dfa3b
jquigley1003 <jeff.quigley@wavinghi.com>
2022-03-02 21:14:24 -0500

    finish module 2

--- src/app/workshops/tribute-to-donna/module01/module01.page.ts
+++ src/app/workshops/tribute-to-donna/module01/module01.page.ts
@@ -1,7 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
-import { YoutubePlayerWeb } from 'capacitor-youtube-player'; // Web version
-import Player from '@vimeo/player';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-module01',
@@ -11,11 +8,6 @@ import Player from '@vimeo/player';
 export class Module01Page implements OnInit {
   @ViewChild('weedOutButsAudio') weedOutButsAudioRef: ElementRef<HTMLAudioElement>;
   @ViewChild('listTheButsAudio') listTheButsAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('connectedAudio') connectedAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('revelAudio') revelAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('beginningAudio') beginningAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('mudraAudio') mudraAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('practices01Audio') practices01AudioRef: ElementRef<HTMLAudioElement>;
 
   completedModule: boolean;
   showCompleteBtn: boolean;
@@ -40,35 +32,12 @@ export class Module01Page implements OnInit {
   }
 
   async ngAfterViewInit() {
-    this.initializeYoutubePlayerPluginWeb();
     this.weedOutButsAudioRef.nativeElement.onloadedmetadata = (event) => {
       this.audioDuration0101 = this.weedOutButsAudioRef.nativeElement.duration;
     };
-  }
-
-
-  async initializeYoutubePlayerPluginWeb() {
-    // const options1 = {playerId: 'youtube-player1', playerSize: {}, videoId: 'Dou2JUmogvU'};
-    // const result1 = await YoutubePlayerWeb.initialize(options1);
-    // const options2 = {playerId: 'youtube-player2', playerSize: {width: 640, height: 360}, videoId: 'aTq2cMQpPeY'};
-    // const result2 = await YoutubePlayerWeb.initialize(options2);
-    // const options3 = {playerId: 'youtube-player3', playerSize: {width: 640, height: 360}, videoId: 'HpvK9Af4aiA'};
-    // const result3 = await YoutubePlayerWeb.initialize(options3);
-    // const options4 = {playerId: 'youtube-player4', playerSize: {width: 640, height: 360}, videoId: 'P3fIZuW9P_M'};
-    // const result4 = await YoutubePlayerWeb.initialize(options4);
-    // const options5 = {playerId: 'youtube-player5', playerSize: {width: 640, height: 360}, videoId: 'GWIhP1_75p4'};
-    // const result5 = await YoutubePlayerWeb.initialize(options5);
-    // const options6 = {playerId: 'youtube-player6', playerSize: {width: 640, height: 360}, videoId: 'aeKYip-sBjM'};
-    // const result6 = await YoutubePlayerWeb.initialize(options6);
-  }
-
-  async destroyYoutubePlayerPluginWeb() {
-    // const result1 = await YoutubePlayerWeb.destroy('youtube-player1');
-    // const result2 = await YoutubePlayerWeb.destroy('youtube-player2');
-    // const result3 = await YoutubePlayerWeb.destroy('youtube-player3');
-    // const result4 = await YoutubePlayerWeb.destroy('youtube-player4');
-    // const result5 = await YoutubePlayerWeb.destroy('youtube-player5');
-    // const result6 = await YoutubePlayerWeb.destroy('youtube-player6');
+    this.listTheButsAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration0102 = this.listTheButsAudioRef.nativeElement.duration;
+    };
   }
 
   markComplete() {
@@ -79,13 +48,5 @@ export class Module01Page implements OnInit {
   markIncomplete() {
     this.showCompleteBtn = false;
     this.showIncompleteBtn = true;
-  }
-
-  ionViewWillLeave() {
-    this.destroyYoutubePlayerPluginWeb();
-  }
-
-  ionViewDidEnter() {
-    this.initializeYoutubePlayerPluginWeb();
   }
 }
--- src/app/workshops/tribute-to-donna/module02/module02.page.ts
+++ src/app/workshops/tribute-to-donna/module02/module02.page.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { YoutubePlayerWeb } from 'capacitor-youtube-player'; // Web version
+import Player from '@vimeo/player';
 
 @Component({
   selector: 'app-module02',
   templateUrl: './module02.page.html',
   styleUrls: ['./module02.page.scss'],
 })
-export class Module02Page implements OnInit {
+export class Module02Page implements OnInit, AfterViewInit {
+  @ViewChild('lovingYourselfAudio') lovingYourselfAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('clarifyAudio') clarifyAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('communeWithMichaelAudio') communeWithMichaelAudioRef: ElementRef<HTMLAudioElement>;
+
+  completedModule: boolean;
+  showCompleteBtn: boolean;
+  showIncompleteBtn = true;
+  assignment0201: boolean;
+  assingment0202: boolean;
+  assingment0203: boolean;
+  assingment0204: boolean;
+  assingment0205: boolean;
+  assingment0206: boolean;
+  assingment0207: boolean;
+  assingment0208: boolean;
+  assingment0209: boolean;
+  assingment0210: boolean;
+  assingment0211: boolean;
+  assingment0212: boolean;
+  assingment0213: boolean;
+  audioDuration0201: number;
+  audioDuration0202: number;
+  audioDuration0203: number;
+ 
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  async ngAfterViewInit() {
+    this.initializeYoutubePlayerPluginWeb();
+    this.lovingYourselfAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration0201 = this.lovingYourselfAudioRef.nativeElement.duration;
+    };
+    this.clarifyAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration0202 = this.clarifyAudioRef.nativeElement.duration;
+    };
+    this.communeWithMichaelAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration0203 = this.communeWithMichaelAudioRef.nativeElement.duration;
+    };
+  }
+
+
+  async initializeYoutubePlayerPluginWeb() {
+    const options1 = {playerId: 'youtube-player0201', playerSize: {}, videoId: 'XLFEvHWD_NE'};
+    const result1 = await YoutubePlayerWeb.initialize(options1);
+  }
+
+  async destroyYoutubePlayerPluginWeb() {
+    const result1 = await YoutubePlayerWeb.destroy('youtube-player0201');
+  }
+
+  markComplete() {
+    this.showCompleteBtn = true;
+    this.showIncompleteBtn = false;
+  }
+
+  markIncomplete() {
+    this.showCompleteBtn = false;
+    this.showIncompleteBtn = true;
+  }
+
+  ionViewWillLeave() {
+    this.destroyYoutubePlayerPluginWeb();
+  }
+
+  ionViewDidEnter() {
+    this.initializeYoutubePlayerPluginWeb();
+  }
 }
