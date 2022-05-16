a9911d87aa2b264ad43415e66a33348c9de8a1b6
jquigley1003 <jeff.quigley@wavinghi.com>
2022-05-16 14:53:27 -0400

    complete module 16

--- src/app/workshops/marrying-yourself/module16/module16.page.ts
+++ src/app/workshops/marrying-yourself/module16/module16.page.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { YoutubePlayerWeb } from 'capacitor-youtube-player'; // Web version
-import Player from '@vimeo/player';
 import { gsap } from 'gsap';
 
 @Component({
@@ -12,16 +11,9 @@ import { gsap } from 'gsap';
 export class Module16Page implements OnInit, AfterViewInit {
   @ViewChild('intentionsVowsAudio') intentionsVowsAudioRef: ElementRef<HTMLAudioElement>;
   @ViewChild('guidanceVowsAudio') guidanceVowsAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('namingYourselfAudio') namingYourselfAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('breathingExerciseAudio') breathingExerciseAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('namingGoddessAudio') namingGoddessAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('experiencingDivineAudio') experiencingDivineAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('siddharthaAudio') siddharthaAudioRef: ElementRef<HTMLAudioElement>;
-  // @ViewChild('guidedLoveAudio') guidedLoveAudioRef: ElementRef<HTMLAudioElement>;
-
-  // @ViewChild('chavonneVerticalVimeo') chavonneVerticalVimeoRef: ElementRef;
-  // @ViewChild('moreThoughtsVimeo') moreThoughtsVimeoRef: ElementRef;
-
+  @ViewChild('preparingAudio') preparingAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('ideasCeremonyAudio') ideasCeremonyAudioRef: ElementRef<HTMLAudioElement>;
+  @ViewChild('futureMarriedAudio') futureMarriedAudioRef: ElementRef<HTMLAudioElement>;
 
   completedModule: boolean;
   showCompleteBtn: boolean;
@@ -29,14 +21,9 @@ export class Module16Page implements OnInit, AfterViewInit {
   audioDuration1601: number;
   audioDuration1602: number;
   audioDuration1603: number;
-  // audioDuration1604: number;
-  // audioDuration1605: number;
-  // audioDuration1606: number;
-  // audioDuration1607: number;
-  // audioDuration1608: number;
+  audioDuration1604: number;
+  audioDuration1605: number;
   assignment1601: boolean;
-  
-  chavonneVerticalVimeoPlayer: Player;
   tl1 = null;
 
   constructor() { }
@@ -67,67 +54,24 @@ export class Module16Page implements OnInit, AfterViewInit {
     this.guidanceVowsAudioRef.nativeElement.onloadedmetadata = (event) => {
       this.audioDuration1602 = this.guidanceVowsAudioRef.nativeElement.duration;
     };
-    // this.namingYourselfAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration1503 = this.namingYourselfAudioRef.nativeElement.duration;
-    // };
-    // this.breathingExerciseAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration1504 = this.breathingExerciseAudioRef.nativeElement.duration;
-    // };
-    // this.namingGoddessAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration1505 = this.namingGoddessAudioRef.nativeElement.duration;
-    // };
-    // this.experiencingDivineAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration1506 = this.experiencingDivineAudioRef.nativeElement.duration;
-    // };
-    // this.siddharthaAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration1507 = this.siddharthaAudioRef.nativeElement.duration;
-    // };
-    // this.guidedLoveAudioRef.nativeElement.onloadedmetadata = (event) => {
-    //   this.audioDuration1508 = this.guidedLoveAudioRef.nativeElement.duration;
-    // };
-    // this.chavonneVerticalVimeoPlayer = new Player(this.chavonneVerticalVimeoRef.nativeElement, {
-    //   id: 696593721,
-    //   height: 350
-    // });
+    this.preparingAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration1603 = this.preparingAudioRef.nativeElement.duration;
+    };
+    this.ideasCeremonyAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration1604 = this.ideasCeremonyAudioRef.nativeElement.duration;
+    };
+    this.futureMarriedAudioRef.nativeElement.onloadedmetadata = (event) => {
+      this.audioDuration1605 = this.futureMarriedAudioRef.nativeElement.duration;
+    };
   }
 
   async initializeYoutubePlayerPluginWeb() {
-    // const player1 = {playerId: 'youtube-player1501', playerSize: {width: 640, height: 360}, videoId: 'RXU0ywJJb8E'};
-    // const result1 = await YoutubePlayerWeb.initialize(player1);
-    // const player2 = {playerId: 'youtube-player1502', playerSize: {width: 640, height: 360}, videoId: 'e11zx5TzRxo'};
-    // const result2 = await YoutubePlayerWeb.initialize(player2);
-    // const player3 = {playerId: 'youtube-player1503', playerSize: {width: 640, height: 360}, videoId: 'D4HxOinvaos'};
-    // const result3 = await YoutubePlayerWeb.initialize(player3);
-    // const player4 = {playerId: 'youtube-player1504', playerSize: {width: 640, height: 360}, videoId: '4cL20-bscdM'};
-    // const result4 = await YoutubePlayerWeb.initialize(player4);
-    // const player5 = {playerId: 'youtube-player1505', playerSize: {width: 640, height: 360}, videoId: 'OPT7687_yko'};
-    // const result5 = await YoutubePlayerWeb.initialize(player5);
-    // const player6 = {playerId: 'youtube-player1506', playerSize: {width: 640, height: 360}, videoId: 'FQ6-lu7GhV0'};
-    // const result6 = await YoutubePlayerWeb.initialize(player6);
-    // const player7 = {playerId: 'youtube-player1507', playerSize: {width: 640, height: 360}, videoId: 'GOVYDIHeecA'};
-    // const result7 = await YoutubePlayerWeb.initialize(player7);
-    // const player8 = {playerId: 'youtube-player1508', playerSize: {width: 640, height: 360}, videoId: 'OOgpT5rEKIU'};
-    // const result8 = await YoutubePlayerWeb.initialize(player8);
-    // const player9 = {playerId: 'youtube-player1509', playerSize: {width: 640, height: 360}, videoId: 'lwgr_IMeEgA'};
-    // const result9 = await YoutubePlayerWeb.initialize(player9);
-    // const player10 = {playerId: 'youtube-player1510', playerSize: {width: 640, height: 360}, videoId: '4gUY9QshcVQ'};
-    // const result10 = await YoutubePlayerWeb.initialize(player10);
-    // const player11 = {playerId: 'youtube-player1511', playerSize: {width: 640, height: 360}, videoId: 'JvljlbKsL2Y'};
-    // const result11 = await YoutubePlayerWeb.initialize(player11);
+    const player1 = {playerId: 'youtube-player1601', playerSize: {width: 640, height: 360}, videoId: 'iQJ7b_xfF2s'};
+    const result1 = await YoutubePlayerWeb.initialize(player1);
   }
 
   async destroyYoutubePlayerPluginWeb() {
-    // const result1 = await YoutubePlayerWeb.destroy('youtube-player1501');
-    // const result2 = await YoutubePlayerWeb.destroy('youtube-player1502');
-    // const result3 = await YoutubePlayerWeb.destroy('youtube-player1503');
-    // const result4 = await YoutubePlayerWeb.destroy('youtube-player1504');
-    // const result5 = await YoutubePlayerWeb.destroy('youtube-player1505');
-    // const result6 = await YoutubePlayerWeb.destroy('youtube-player1506');
-    // const result7 = await YoutubePlayerWeb.destroy('youtube-player1507');
-    // const result8 = await YoutubePlayerWeb.destroy('youtube-player1508');
-    // const result9 = await YoutubePlayerWeb.destroy('youtube-player1509');
-    // const result10 = await YoutubePlayerWeb.destroy('youtube-player1510');
-    // const result11 = await YoutubePlayerWeb.destroy('youtube-player1511');
+    const result1 = await YoutubePlayerWeb.destroy('youtube-player1601');
   }
 
   onToggle() {
